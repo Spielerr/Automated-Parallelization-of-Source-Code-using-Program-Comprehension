d9c6ad80b1229e24a3cf80d4e25f17362f1808d1
Mayur Peshve <mayurpeshve2@gmail.com>
2021-03-21 20:37:08 +0530

    Metric changes

--- Phase1/demo_2/metric.py
+++ Phase1/demo_2/metric.py
@@ -2,37 +2,45 @@ import argparse
 import subprocess
 import time
 import sys
+from timeit import timeit
 
-parallelCode = sys.argv[2]
 sequentialCode = sys.argv[1]
+parallelCode = sys.argv[2]
 
 
-testcase_size = 1000
+testcase_size = 50000000
 
 seqTime = []
 parallelTime = []
 
 
-for _ in range(100):
-    begin = time.time()
+for _ in range(2):
+
+    begin = time.perf_counter()
     seq = subprocess.Popen(
-        ["./" + sequentialCode], stdin=subprocess.PIPE, stdout=subprocess.PIPE
+        ["./" + sequentialCode, str(50000000)], stdin=subprocess.PIPE, stdout=subprocess.PIPE
     )
     seq.stdin.write(bytes(str(testcase_size), "utf-8"))
-    seq.communicate()
+    # seq.communicate(input = (str(testcase_size)).encode('utf-8'))
+    out, err = seq.communicate()
+    print(out.decode('utf-8'), err)
+    end = time.perf_counter()
     seq.stdin.close()
-    end = time.time()
     seqTime.append(end - begin)
 
-    begin = time.time()
+    begin = time.perf_counter()
 
     para = subprocess.Popen(
-        ["./" + parallelCode], stdin=subprocess.PIPE, stdout=subprocess.PIPE
+        ["./" + parallelCode, str(50000000)], stdin=subprocess.PIPE, stdout=subprocess.PIPE
     )
     para.stdin.write(bytes(str(testcase_size), "utf-8"))
-    para.communicate()
+    # para.communicate(input = (str(testcase_size)).encode('utf-8'))
+    out, err = para.communicate()
+    print(out, err)
     para.stdin.close()
-    end = time.time()
+    end = time.perf_counter()
+
+    # para.stdin.close()
     parallelTime.append(end - begin)
 
     testcase_size *= 10
