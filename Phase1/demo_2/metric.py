3adbf2a2c93da722b0c3c892dbaf0267ec673176
ManuMBhat <ManuMBhat@github.com>
2021-03-21 21:27:15 +0530

    metric working, sorta

--- Phase1/demo_2/metric.py
+++ Phase1/demo_2/metric.py
@@ -1,4 +1,4 @@
-import argparse
+import resource
 import subprocess
 import time
 import sys
@@ -8,42 +8,46 @@ sequentialCode = sys.argv[1]
 parallelCode = sys.argv[2]
 
 
-testcase_size = 50000000
+testcase_size = 500
 
-seqTime = []
-parallelTime = []
+seqTime = {}
+parallelTime = {}
 
 
-for _ in range(2):
+for _ in range(3):
 
-    begin = time.perf_counter()
+    begin = resource.getrusage(resource.RUSAGE_CHILDREN)
     seq = subprocess.Popen(
-        ["./" + sequentialCode, str(50000000)], stdin=subprocess.PIPE, stdout=subprocess.PIPE
+        ["./" + sequentialCode, str(testcase_size).encode("utf-8")],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
     )
-    seq.stdin.write(bytes(str(testcase_size), "utf-8"))
+    # seq.stdin.write(bytes(str(testcase_size), "utf-8"))
     # seq.communicate(input = (str(testcase_size)).encode('utf-8'))
     out, err = seq.communicate()
-    print(out.decode('utf-8'), err)
-    end = time.perf_counter()
-    seq.stdin.close()
-    seqTime.append(end - begin)
 
-    begin = time.perf_counter()
+    end = resource.getrusage(resource.RUSAGE_CHILDREN)
+    seq.stdin.close()
+    seqTime[str(testcase_size)] = end.ru_utime - begin.ru_utime
+
+    begin = resource.getrusage(resource.RUSAGE_CHILDREN)
 
     para = subprocess.Popen(
-        ["./" + parallelCode, str(50000000)], stdin=subprocess.PIPE, stdout=subprocess.PIPE
+        ["./" + parallelCode, str(testcase_size).encode("utf-8")],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
     )
-    para.stdin.write(bytes(str(testcase_size), "utf-8"))
+    # para.stdin.write(bytes(str(testcase_size), "utf-8"))
     # para.communicate(input = (str(testcase_size)).encode('utf-8'))
     out, err = para.communicate()
-    print(out, err)
+
     para.stdin.close()
-    end = time.perf_counter()
+    end = resource.getrusage(resource.RUSAGE_CHILDREN)
 
     # para.stdin.close()
-    parallelTime.append(end - begin)
+    parallelTime[str(testcase_size)] = end.ru_utime - begin.ru_utime
 
-    testcase_size *= 10
+    testcase_size *= 1000
 
 print(seqTime)
 print(parallelTime)
--- Phase1/demo_2/seq_test.c
+++ Phase1/demo_2/seq_test.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-double mean(int* a1, int n1)
-{
-double mean = 0;
-for(int i = 0;i<n1;++i)
-{
-mean += a1[i];
-}
-mean /= n1;
-return mean;
+double mean(int *a1, int n1) {
+    double mean = 0;
+    for (int i = 0; i < n1; ++i) {
+        mean += a1[i];
+    }
+    mean /= n1;
+    return mean;
 }
 
 // void sort(int* arr, int arr_n)
@@ -29,7 +27,6 @@ return mean;
 // }
 // }
 
-
 // int median(int* a2, int n2)
 // {
 // sort(a2,n2);
@@ -43,73 +40,59 @@ return mean;
 
 // }
 
-
-int search(int *a3, int n3, int key)
-{
-for(int i = 0;i<n3;++i)
-{
-if(a3[i]==key)
-{
-return i;
-}
-}
-return -1;
+int search(int *a3, int n3, int key) {
+    for (int i = 0; i < n3; ++i) {
+        if (a3[i] == key) {
+            return i;
+        }
+    }
+    return -1;
 }
 
-int max(int *a4, int n4)
-{
-int max = a4[0];
-for(int i = 1; i<n4; ++i)
-{
-if(a4[i]>max)
-{
-max = a4[i];
-}
+int max(int *a4, int n4) {
+    int max = a4[0];
+    for (int i = 1; i < n4; ++i) {
+        if (a4[i] > max) {
+            max = a4[i];
+        }
+    }
+
+    return max;
 }
 
-return max;
+int min(int *a4, int n4) {
+    int min = a4[0];
+    for (int i = 1; i < n4; ++i) {
+        if (a4[i] < min) {
+            min = a4[i];
+        }
+    }
+
+    return min;
 }
 
-int min(int *a4, int n4)
-{
-int min = a4[0];
-for(int i = 1; i<n4; ++i)
-{
-if(a4[i]<min)
-{
-min = a4[i];
-}
+int factorial(int n) {
+    int prod = 1;
+    for (int i = n; i >= 1; i--) {
+        prod *= i;
+    }
+    return prod;
 }
 
-return min;
-}
+int main(int argc, char *argv[]) {
+    int array_size = atoi(argv[1]);
+    int *array = (int *)malloc(array_size * sizeof(int));
 
-int factorial(int n)
-{
-int prod=1;
-for(int i=n;i>=1;i--)
-{
-prod*=i;
-}
-return prod;
-}
-
-
-int main()
-{
-   int *array = (int *) malloc(500000000 * sizeof(int));
-   int array_size = 500000000;
-   srand(8);
-   for(int i = 0; i < array_size; ++i) {
-      array[i] = rand();
-   }
-double m1; 
-int m2, m3, m4, k;
-m1 = mean(array, array_size);
-// m2 = median(array, array_size);
-// sort(array, array_size);
-m3 = max(array, array_size);
-m4 = min(array, array_size);
-// k = search(array,array_size,4);
-
+    srand(8);
+    for (int i = 0; i < array_size; ++i) {
+        array[i] = rand();
+    }
+    double m1;
+    int m2, m3, m4, k;
+    m1 = mean(array, array_size);
+    // m2 = median(array, array_size);
+    // sort(array, array_size);
+    m3 = max(array, array_size);
+    m4 = min(array, array_size);
+    // k = search(array,array_size,4);
 }
