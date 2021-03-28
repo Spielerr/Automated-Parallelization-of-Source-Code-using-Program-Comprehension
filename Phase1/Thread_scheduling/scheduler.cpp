48ed306c6110c1be408ad936bce8267306111660
Darshan D <darshand2000@gmail.com>
2021-03-28 13:06:29 +0530

    format code

--- Phase1/Thread_scheduling/scheduler.cpp
+++ Phase1/Thread_scheduling/scheduler.cpp
@@ -5,29 +5,47 @@
 #include <thread>
 using namespace std;
 
-bool check_key(map<string, int>& m, string& key) { return !(m.find(key) == m.end()); }
+bool check_key(map<string, int>& m, string& key) 
+{ 
+    return !(m.find(key) == m.end()); 
+}
 
 // checks if function makes any write to the paramaters
 template <typename ptr_t1, typename ptr_t2>
-bool is_writing_fn(ptr_t1 order, ptr_t2 write_fns, int fn_num, int write_fn_num) {
-    while (write_fn_num <= fn_num) {
-        if (order->first == write_fns->first) return true;
+bool is_writing_fn(ptr_t1 order, ptr_t2 write_fns, int fn_num, int write_fn_num) 
+{
+    while (write_fn_num <= fn_num) 
+    {
+        if (order->first == write_fns->first) 
+        {
+            return true;
+        }
+
         ++write_fns;
         ++write_fn_num;
     }
+
     return false;
 }
+
+
 // checks if no paramter of the function is written into
-bool no_write_params(pair<string, vector<string>>& item, map<string, int>& vars_write) {
-    for (auto it = item.second.begin(); it != item.second.end(); ++it) {
-        if (check_key(vars_write, *it) && vars_write[*it] > 0) {
+bool no_write_params(pair<string, vector<string>>& item, map<string, int>& vars_write) 
+{
+    for (auto it = item.second.begin(); it != item.second.end(); ++it) 
+    {
+        if (check_key(vars_write, *it) && vars_write[*it] > 0) 
+        {
             return false;
         }
     }
+
     return true;
 }
 
-int main() {
+
+int main() 
+{
     multimap<string, vector<string>> deps;
 
     vector<pair<string, vector<string>>> vec_deps;
@@ -38,22 +56,29 @@ int main() {
     string fn_call;
 
     ifstream MyReadFile1("input.txt");
-    while (getline(MyReadFile1, dependencies)) {
+    while (getline(MyReadFile1, dependencies)) 
+    {
         vector<string> result;
         stringstream s_stream(dependencies);
-        while (s_stream.good()) {
+        while (s_stream.good()) 
+        {
             string substr;
             getline(s_stream, substr, ',');
             result.push_back(substr);
         }
+
         pair<string, vector<string>> temp;
         temp.first = result[0];
-        for (int i = 1; i < result.size(); i++) {
+
+        for (int i = 1; i < result.size(); i++) 
+        {
             temp.second.push_back(result[i]);
-            if (!check_key(var_write_count, result[i])) {
+            if (!check_key(var_write_count, result[i])) 
+            {
                 var_write_count[result[i]] = 0;
             }
         }
+
         deps.insert(temp);
         vec_deps.push_back(temp);
     }
@@ -63,52 +88,73 @@ int main() {
 
     ifstream MyReadFile2("input2.txt");
     vector<pair<string, vector<string>>> order;
-    while (getline(MyReadFile2, fn_call)) {
+    while (getline(MyReadFile2, fn_call)) 
+    {
         vector<string> result;
         stringstream s_stream(fn_call);
-        while (s_stream.good()) {
+        while (s_stream.good())
+        {
             string substr;
             getline(s_stream, substr, ',');
             result.push_back(substr);
         }
+
         pair<string, vector<string>> temp;
         temp.first = result[0];
-        for (int i = 1; i < result.size(); i++) {
+        for (int i = 1; i < result.size(); i++) 
+        {
             temp.second.push_back(result[i]);
         }
 
         order.push_back(temp);
     }
 
-    vector<pair<string, vector<string>>> my_order;              // reordered fn call
-    vector<pair<string, vector<string>>> temp_write_dep_order;  // temp to maintain order of fn that do not have write operations
-    map<string, int> vars_write(var_write_count);               // number of writes done on given variable
+    // reordered fn call
+    vector<pair<string, vector<string>>> my_order;              
+
+    // temp to maintain order of fn that do not have write operations
+    vector<pair<string, vector<string>>> temp_write_dep_order;  
+
+    // number of writes done on given variable
+    map<string, int> vars_write(var_write_count);  
+
     auto iterator_my_order = my_order.begin();
     auto iterator_order = order.begin();
     auto iterator_write_order = vec_deps.begin();
 
     int fn_num = 0;
     int write_fn_num = 0;
-    while (iterator_order != order.end()) {
+
+    while (iterator_order != order.end()) 
+    {
         // if no paramter needs any write operations, then add to my_order as it can be executed
-        if (no_write_params(*iterator_order, vars_write)) {
+        if (no_write_params(*iterator_order, vars_write)) 
+        {
             my_order.push_back(*iterator_order);
-        } else {
+        } 
+        else 
+        {
             // maintain original order of fns that need write to occur in temp vector
             temp_write_dep_order.push_back(*iterator_order);
         }
+
         // computes number of writes have occured to a given variable
-        if (is_writing_fn(iterator_order, iterator_write_order, fn_num, write_fn_num)) {
-            for (auto it = iterator_write_order->second.begin(); it != iterator_write_order->second.end(); ++it) {
+        if (is_writing_fn(iterator_order, iterator_write_order, fn_num, write_fn_num)) 
+        {
+            for (auto it = iterator_write_order->second.begin(); it != iterator_write_order->second.end(); ++it) 
+            {
                 vars_write[*it] += 1;
             }
+
             cout << endl;
             write_fn_num++;
             ++iterator_write_order;
         }
+
         ++iterator_order;
         fn_num++;
     }
+
     // insert the temp fn order into final reordered fn order
     my_order.insert(my_order.end(), temp_write_dep_order.begin(), temp_write_dep_order.end());
 
@@ -118,22 +164,28 @@ int main() {
     fn_num = 0;
     write_fn_num = 0;
 
-    while (iterator_order_of_exec != my_order.end()) {
+    while (iterator_order_of_exec != my_order.end()) 
+    {
         // sets future (wait for write) for all variables needed for current function
-        for (auto it = iterator_order_of_exec->second.begin(); it != iterator_order_of_exec->second.end(); ++it) {
-            if (var_write_count.find(*it) != var_write_count.end() && var_write_count[*it] != 0) {
+        for (auto it = iterator_order_of_exec->second.begin(); it != iterator_order_of_exec->second.end(); ++it) 
+        {
+            if (var_write_count.find(*it) != var_write_count.end() && var_write_count[*it] != 0) 
+            {
                 cout << "std::future<void> f_" << *it << "_" << var_write_count[*it] << "= p_" << *it << "_" << var_write_count[*it] - 1
                      << ".get_future();\n";
                 var_write_count[*it] -= 1;
             }
         }
         // Create promise for all the variable the current function will write to
-        if (is_writing_fn(iterator_order_of_exec, iterator_write_order, fn_num, write_fn_num)) {
-            for (auto it = iterator_write_order->second.begin(); it != iterator_write_order->second.end(); ++it) {
+        if (is_writing_fn(iterator_order_of_exec, iterator_write_order, fn_num, write_fn_num)) 
+        {
+            for (auto it = iterator_write_order->second.begin(); it != iterator_write_order->second.end(); ++it) 
+            {
                 cout << "std::promise<void> p_" << *it << "_" << var_write_count[*it];
                 vars_for_future[*it] += 1;
                 var_write_count[*it] += 1;
             }
+            
             cout << endl;
             write_fn_num++;
             ++iterator_write_order;
