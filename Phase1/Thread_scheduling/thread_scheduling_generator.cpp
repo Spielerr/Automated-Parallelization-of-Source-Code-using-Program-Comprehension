2a9cf5c1f8ffc31e3e453aae4a9ecc92468c61c0
Darshan D <darshand2000@gmail.com>
2021-04-04 17:25:50 +0530

    second initial commit for thread scheduling generator program

--- Phase1/Thread_scheduling/thread_scheduling_generator.cpp
+++ Phase1/Thread_scheduling/thread_scheduling_generator.cpp
@@ -24,37 +24,45 @@ void prologue()
     cout<<"vector<future<int>> int_futures;\n";
     cout<<"vector<pair<int, string>> special;\n";
     cout<<"vector<pair<int, atomic<bool>&>> thread_track;\n";
+
+    //change arr1 and n based on the output files from lara
     cout<<"vector<vector<string>> args{{\"arr1\",\"n\"}, {\"arr1\",\"n\"}, {\"arr1\",\"n\"}, {\"arr2\",\"n\"}, {\"arr2\",\"n\"}, {\"arr2\",\"n\"}};\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n";
+
+    cout<<"deque<pair<int, std::function<int(int)>>> ready_queue;\n";
+    cout<<"deque<pair<int, std::function<int(int)>>> wait_queue;\n";
+    cout<<"atomic<int> special_changed(0);\n";
+    cout<<"string remove_fn;\n\n";
+
+    cout<<"class Find_special\n";
+    cout<<"{\n";
+    cout<<"     private:\n";
+    cout<<"     int x_;\n";
+    cout<<"     public:\n";
+    cout<<"     Find_special(int x) : x_(x) {}\n";
+    cout<<"     bool operator()(pair<int, string> i)\n";
+    cout<<"     {\n";
+    cout<<"          return i.first == x_;\n";
+    cout<<"     }\n";
+    cout<<"};\n\n";
+
+
+    cout<<"class Find_special2\n";
+    cout<<"{\n";
+    cout<<"     private:\n";
+    cout<<"     string x_;\n";
+    cout<<"     public:\n";
+    cout<<"     Find_special2(string x) : x_(x) {}\n";
+    cout<<"     bool operator()(pair<int, string> i)\n";
+    cout<<"     {\n";
+    cout<<"          if(x_.compare(i.second) == 0)\n";
+    cout<<"          {\n";
+    cout<<"              return true;\n";
+    cout<<"          }\n";
+    cout<<"          return false;\n";
+    cout<<"     }\n";
+    cout<<"};\n";
+
+    
     cout<<"\n";
     cout<<"\n";
     cout<<"\n";
