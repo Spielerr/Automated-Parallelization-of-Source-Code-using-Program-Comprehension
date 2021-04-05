de198a59ddfcb3bc33aded9aea88bae47fc4c819
Spielerr <kkgstud007@gmail.com>
2021-04-05 18:12:55 +0530

    client demo

---
+++ Phase1/Thread_scheduling/client_demo.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <sys/time.h>
+#include "prog.hpp"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int input_n = atoi(argv[1]);
+	int *arr1 = (int *)malloc(input_n * sizeof(int));
+    int array_size1 = input_n;
+    srand(8);
+    for(int i = 0; i < array_size1; ++i)
+    {    
+        arr1[i] = rand();
+    }
+
+	int *arr2 = (int *)malloc(input_n * sizeof(int));
+    int array_size2 = input_n;
+    srand(2);
+    for(int i = 0; i < array_size2; ++i)
+    {    
+        arr2[i] = rand();
+    }
+
+	int n = array_size1;
+
+	struct timeval stop, start;
+    gettimeofday(&start, NULL);
+
+	my_sort(arr1, n);
+	my_min(arr1, n);
+	my_max(arr1, n);
+	my_sort(arr2, n);
+	my_min(arr2, n);
+	my_max(arr2, n);
+
+	gettimeofday(&stop, NULL);
+    printf("took %lu ms\n", ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec)/1000);
+
+	return 0;
+}
