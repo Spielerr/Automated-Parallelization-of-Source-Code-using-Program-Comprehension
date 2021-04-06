e2a70beebb313e51e4907d4f95712a58dd87b6d7
mayurpeshve2@gmail.com <mayurpeshve2@gmail.com>
2021-04-06 22:23:49 +0530

    Perform Synchronisation

--- Phase1/Thread_scheduling/client_3_threads.cpp
+++ Phase1/Thread_scheduling/client_3_threads.cpp
@@ -11,10 +11,17 @@
 #include <mutex>
 
 
+mutex m_sp;
+mutex m_wq;
+mutex m_rq;
+mutex m_tt;
+mutex m_tf;
+mutex m_sf;
+mutex m_sp_c;
 
 // {
 //   std::mutex m,
-//   std::lock_guard<std::mutex> lockGuard(m);
+//   std::lock_guard<std::mutex> lockGuard(m_sp);
 //   sharedVariable= getVar();
 // }
 
@@ -26,11 +33,11 @@ using namespace thread_pool;
 int num_threads = thread::hardware_concurrency();
 ThreadPool tp(num_threads);
 // ThreadPool tp{};
-int sp_c = 0;
-int wr_q = 0;
+int sp_c = 2;
+int wr_q = 4;
 
-vector<future<void>> void_futures;
-vector<future<int>> int_futures;
+vector<shared_future<void>> void_futures;
+vector<shared_future<int>> int_futures;
 
 //stores the arguments for those functions which are changing them
 vector<pair<int, string>> special;
@@ -109,27 +116,35 @@ class Find_special3
 void thread_track_fn()
 {
 	int flag = true;
-	while(sp_c)
+	int sp_done_c = 0;
+	while(true)
 	{
 		// cout << "thread_track_fn\n";
-		for(auto x:thread_track)
 		{
-			//update special
-			//while(special_changed)cout << "Busy Waiting" << "\n";
-			if(x.second)
+			lock_guard<mutex> lockGuard(m_tt);
+			for(auto x:thread_track)
 			{
-					//should prolly call .get here - need to get the correct future through an iterator here
-					x.second = false;
-					mutex m;
-					lock_guard<mutex> lockGuard(m);
-					auto iter_special = find_if(special.begin(), special.end(), Find_special(x.first));
-					remove_fn = (*iter_special).second;
-					special.erase(iter_special);
-					// cout << "Changing Special" << "\n";
-					special_changed = 1; //make it 1 only when i remove an arg from special
-					// can also remove x from the thread_track vector, that way eliminate iteration through it everytime
-					--sp_c;
+				//update special
+				while(special_changed)cout << "Busy Waiting" << "\n";
+				if(x.second)
+				{
+						//should prolly call .get here - need to get the correct future through an iterator here
+						x.second = false;
+						lock_guard<mutex> lockGuard(m_sp);
+						auto iter_special = find_if(special.begin(), special.end(), Find_special(x.first));
+						remove_fn = (*iter_special).second;
+						special.erase(iter_special);
+						// cout << "Changing Special" << "\n";
+						special_changed = 1; //make it 1 only when i remove an arg from special
+						// can also remove x from the thread_track vector, that way eliminate iteration through it everytime
+						++sp_done_c;
+				}
 			}
+		}
+		if(sp_done_c == sp_c) 
+		{
+			cout << "EXITING TTF\n";
+			break;
 		}
 		// cout << "Special: " << special.size() << "\n";
 	}
@@ -139,61 +154,75 @@ void thread_track_fn()
 void scheduling_fn()
 {
 	bool flag = true;
+	int wr_done_c = 0;
 	//special got updated
-	while(wr_q)
+	while(true)
 	{
 		// cout << "scheduling_fn\n";
-		while(special_changed)
 		{
-			// if wait queue contains some functions, get only those functions which are dependent on
-			// remove_fn which has just got updated
-			// how to check for these functions? refer the input data
-
-			//push eligible elements from wait queue to ready queue
-
-			mutex m;
-			lock_guard<mutex> lockGuard(m);
-
-			for(auto x:wait_queue)
+			while(special_changed)
 			{
-				//pick out every argument from func in x
-				//check if it exists in special
-				//if it doesnt
-				//	move it to ready queue
-				bool is_found = false;
-				for(auto y:args[x.first - 1])
+				// if wait queue contains some functions, get only those functions which are dependent on
+				// remove_fn which has just got updated
+				// how to check for these functions? refer the input data
+
+				//push eligible elements from wait queue to ready queue
+
+
 				{
-					if(find_if(begin(special), end(special), Find_special2(y)) != end(special))
-					{	
-						is_found = true;
-						break;
+					lock_guard<mutex> lockGuard(m_wq);
+					for(auto x:wait_queue)
+					{
+						//pick out every argument from func in x
+						//check if it exists in special
+						//if it doesnt
+						//	move it to ready queue
+						bool is_found = false;
+						for(auto y:args[x.first - 1])
+						{
+							lock_guard<mutex> lockGuard(m_sp);
+							if(find_if(begin(special), end(special), Find_special2(y)) != end(special))
+							{	
+								is_found = true;
+								break;
+							}
+						}
+						if(!is_found)
+						{
+							wait_queue.erase(find_if( begin(wait_queue), end(wait_queue), Find_special3<pair<int, std::function<int()>>>(x.first)));
+							lock_guard<mutex> lockGuard(m_rq);
+							ready_queue.push_back(x);
+						}
 					}
 				}
-				if(!is_found)
-				{
-					wait_queue.erase(find_if( begin(wait_queue), end(wait_queue), Find_special3<pair<int, std::function<int()>>>(x.first)));
-					ready_queue.push_back(x);
-				}
+				special_changed = 0;
 			}
-			special_changed = 0;
 		}
 
-		if(ready_queue.size() != 0)
 		{
-			for(auto &x:ready_queue)
+			lock_guard<mutex> lockGuard(m_rq);
+			if(ready_queue.size() != 0)
 			{
-				int_futures.emplace_back(tp.Submit(x.second));
+				for(auto &x:ready_queue)
+				{
+					int_futures.emplace_back(tp.Submit(x.second));
 
-				auto iter = find_if( begin(ready_queue), end(ready_queue), Find_special3<pair<int, std::function<int()>>>(x.first));
-				ready_queue.erase(iter);
-				--wr_q;
-				//file output code
-				// int_futures.emplace_back(tp.Submit(min, cref(arr1), cref(n)));
+					auto iter = find_if( begin(ready_queue), end(ready_queue), Find_special3<pair<int, std::function<int()>>>(x.first));
+					ready_queue.erase(iter);
+					++wr_done_c;
+					//file output code
+					// int_futures.emplace_back(tp.Submit(min, cref(arr1), cref(n)));
+				}
 			}
 		}
 		// cout << "Ready Queue: " << ready_queue.size() << "\n";
 		// cout << "Special: " << special.size() << "\n";
 		// cout << "Wait Queue: " << wait_queue.size() << "\n";
+		if(wr_done_c == wr_q) 
+		{
+			cout << "EXITING SF\n";
+			break;
+		}
 	}
 }
 
@@ -248,8 +277,10 @@ int main(int argc, char **argv)
 	// if my_sort is writing into one or more of its arguments, add them to special
 	// file write code for that
 	pair<int,string> p1_1(1,"arr1");
-	special.push_back(p1_1);
-	++sp_c;
+	{
+		lock_guard<mutex> lockGuard(m_sp);
+		special.push_back(p1_1);
+	}
 	// atomic bool variable to track the execution of my_sort(arr1, n)
 	atomic<bool> done1(false);
 
@@ -259,7 +290,11 @@ int main(int argc, char **argv)
     }));
 
 	pair<int, atomic<bool>&> p1_2(1, done1); 
-	thread_track.push_back(p1_2);
+
+	{
+		lock_guard<mutex> lockGuard(m_tt);
+		thread_track.push_back(p1_2);
+	}
 	
 	
 	// int min1 = min(arr1, n);
@@ -267,37 +302,56 @@ int main(int argc, char **argv)
 	auto fn2 =  bind(::min, arr1, n);
 
 	//min does depend on one of the arguments in the special array, so add them to wait queue
-	if(find_if(begin(special), end(special), Find_special2("arr1")) != end(special))
+	vector<pair<int,string>>::iterator iter1, iter2;
+	
 	{
+		lock_guard<mutex> lockGuard(m_sp);
+		iter1 = find_if(begin(special), end(special), Find_special2("arr1"));
+		iter2 = end(special);
+	}
+	if( iter1 != iter2 )
+	{
+		lock_guard<mutex> lockGuard(m_wq);
 		wait_queue.push_back(pair<int, std::function<int()>>(2, fn2));
 		// ready_queue.push_back(pair<int, std::function<void()>>(2, [&](){ min1 = min(arr1, n); }));
 	}
 	// push to wait queue
 	else
 	{
+		lock_guard<mutex> lockGuard(m_rq);
 		ready_queue.push_back(pair<int, std::function<int()>>(2, fn2));
 		// ready_queue.push_back(pair<int, std::function<void()>>(2, [&](){ min1 = min(arr1, n); } ));
 	}
-	++wr_q;
 
 	// int max1 = max(arr1, n);
 	auto fn3 = bind(::max, arr1, n);
-	if(find_if(begin(special), end(special), Find_special2("arr1")) != end(special))
+	
 	{
+		lock_guard<mutex> lockGuard(m_sp);
+		iter1 = find_if(begin(special), end(special), Find_special2("arr1"));
+		iter2 = end(special);
+	}
+
+	if(iter1 != iter2)
+	{
+		lock_guard<mutex> lockGuard(m_wq);
 		wait_queue.push_back(pair<int, std::function<int()>>(3, fn3));
 		// ready_queue.push_back(pair<int, std::function<void()>>(2, [&](){ min1 = min(arr1, n); }));
 	}
 	else
 	{
+		lock_guard<mutex> lockGuard(m_rq);
 		ready_queue.push_back(pair<int, std::function<int()>>(3, fn3));
 		// ready_queue.push_back(pair<int, std::function<void()>>(2, [&](){ min1 = min(arr1, n); } ));
 	}
-	++wr_q;
 
 	// my_sort(arr2, n);
 	pair<int,string> p4_1(4,"arr2");
-	special.push_back(p4_1);
-	++sp_c;
+	
+	{
+		lock_guard<mutex> lockGuard(m_sp);
+		special.push_back(p4_1);
+	}
 
 	atomic<bool> done4(false);
 	void_futures.emplace_back(tp.Submit([&] {
@@ -305,50 +359,181 @@ int main(int argc, char **argv)
         done4 = true;
     }));
 	pair<int, atomic<bool>&> p4_2(4, done4);
-	thread_track.push_back(p4_2);
+		
+	{
+		lock_guard<mutex> lockGuard(m_tt);
+		thread_track.push_back(p4_2);
+	}
 
 
 	// int min2 = min(arr2, n);
 	// if arr2 is found in special vector
 	auto fn5 =  bind(::min, arr2, n);
-	if(find_if(begin(special), end(special), Find_special2("arr2")) != end(special))
+
+	{
+		lock_guard<mutex> lockGuard(m_sp);
+		iter1 = find_if(begin(special), end(special), Find_special2("arr2"));
+		iter2 = end(special);
+	}
+
+	if(iter1 != iter2)
 	{
 		//min to queue
+		lock_guard<mutex> lockGuard(m_wq);
 		wait_queue.push_back(pair<int, std::function<int()>>(5, fn5));
 		// ready_queue.push_back(pair<int, std::function<void()>>(2, [&](){ min1 = min(arr1, n); }));
 	}
 	else
 	{
+		lock_guard<mutex> lockGuard(m_rq);
 		ready_queue.push_back(pair<int, std::function<int()>>(5, fn5));
 		// ready_queue.push_back(pair<int, std::function<void()>>(2, [&](){ min1 = min(arr1, n); } ));
 	}
 
-	++wr_q;
 
 	// int max2 = max(arr2, n);
 	auto fn6 =  bind(::max, arr2, n);
-	if(find_if(begin(special), end(special), Find_special2("arr2")) != end(special))
+
+	{
+		lock_guard<mutex> lockGuard(m_sp);
+		iter1 = find_if(begin(special), end(special), Find_special2("arr2"));
+		iter2 = end(special);
+	}
+
+	if(iter1 != iter2)
 	{
 		//min to queue
+		lock_guard<mutex> lockGuard(m_wq);
 		wait_queue.push_back(pair<int, std::function<int()>>(6, fn6));
 		// ready_queue.push_back(pair<int, std::function<void()>>(2, [&](){ min1 = min(arr1, n); }));
 	}
 	else
 	{
+		lock_guard<mutex> lockGuard(m_rq);
 		ready_queue.push_back(pair<int, std::function<int()>>(6, fn6));
 		// ready_queue.push_back(pair<int, std::function<void()>>(2, [&](){ min1 = min(arr1, n); } ));
 	}
-	++wr_q;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	// // int min1 = min(arr1, n);
+	// // int min1 = -999999;
+	// //min does not change any of its arguments
+	// auto fn8 =  bind(::min, arr1, n);
+
+	// //min does depend on one of the arguments in the special array, so add them to wait queue
+	// if(find_if(begin(special), end(special), Find_special2("arr1")) != end(special))
+	// {
+	// 	wait_queue.push_back(pair<int, std::function<int()>>(8, fn8));
+	// 	// wait_queue.push_back(pair<int, std::function<void(int)>>(8, [&](){ min1 = fn8(); }));
+	// }
+	// // push to wait queue
+	// else
+	// {
+	// 	ready_queue.push_back(pair<int, std::function<int()>>(8, fn8));
+	// 	// ready_queue.push_back(pair<int, std::function<void(int)>>(8, [&](){ min1 = fn8(); } ));
+	// }
+	// ++wr_q;
+
+	// // int max1 = max(arr1, n);
+	// // int max1 = -9999999;
+	// auto fn9 = bind(::max, arr1, n);
+	// if(find_if(begin(special), end(special), Find_special2("arr1")) != end(special))
+	// {
+	// 	wait_queue.push_back(pair<int, std::function<int()>>(9, fn9));
+	// 	// ready_queue.push_back(pair<int, std::function<void(int)>>(9, [&](){ max1 = fn9(); }));
+	// }
+	// else
+	// {
+	// 	ready_queue.push_back(pair<int, std::function<int()>>(9, fn9));
+	// 	// ready_queue.push_back(pair<int, std::function<void(int)>>(9, [&](){ max1 = fn9(); } ));
+	// }
+	// ++wr_q;
+
+	// // my_sort(arr2, n);
+
+
+	// // int min2 = min(arr2, n);
+	// // int min2 = -99999;
+	// // if arr2 is found in special vector
+	// auto fn11 =  bind(::min, arr2, n);
+	// if(find_if(begin(special), end(special), Find_special2("arr2")) != end(special))
+	// {
+	// 	//min to queue
+	// 	wait_queue.push_back(pair<int, std::function<int()>>(11, fn11));
+	// 	// ready_queue.push_back(pair<int, std::function<void(int)>>(11, [&](){ min2 = fn11(); }));
+	// }
+	// else
+	// {
+	// 	ready_queue.push_back(pair<int, std::function<int()>>(11, fn11));
+	// 	// ready_queue.push_back(pair<int, std::function<void(int)>>(11, [&](){ min2 = fn11(); } ));
+	// }
+	// ++wr_q;
+
+
+	// // int max2 = max(arr2, n);
+	// // int max2 = -99999;
+	// auto fn12 =  bind(::max, arr2, n);
+	// if(find_if(begin(special), end(special), Find_special2("arr2")) != end(special))
+	// {
+	// 	//min to queue
+	// 	wait_queue.push_back(pair<int, std::function<int()>>(12, fn12));
+	// 	// ready_queue.push_back(pair<int, std::function<void(int)>>(12, [&](){ max2 = fn12(); }));
+	// }
+	// else
+	// {
+	// 	ready_queue.push_back(pair<int, std::function<int()>>(12, fn12));
+	// 	// ready_queue.push_back(pair<int, std::function<void(int)>>(12, [&](){ max2 = fn12(); } ));
+	// }
+	// ++wr_q;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	for(auto &x:void_futures)
 	{
 		x.wait();
+		x.get();
 	}
-
 	// min and max values (called twice after every sort)
 	for(auto &x:int_futures)
 	{
 		x.wait();
+		cout << std::boolalpha;
+		cout << x.valid() << "\n";
+		if(x.valid())
 		cout << x.get() << "\n";
 	}
 	// cout << min1 << "\n\n";
