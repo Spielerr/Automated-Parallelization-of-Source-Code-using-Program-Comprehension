53180623abb5d966b063fbad4edad3eb538b1799
Darshan D <darshand2000@gmail.com>
2021-04-15 09:06:28 +0530

    python regex script to read the input from lara terminal and write into input3.txt

---
+++ Phase1/demo_2/lara/read_lara_o_ip3.py
@@ -0,0 +1,20 @@
+import re
+
+file_ip = open("test_o_lara.txt_2",'r')
+
+ip_str = file_ip.read()
+
+file_ip.close()
+
+file_input = open("input3.txt","w")
+
+
+pat_ip1 = r"Start of Input3.txt\n(.*)\nEnd of Input3.txt"
+input3 = re.search(pat_ip1,ip_str,re.S)
+
+if input3:
+    #print(input3.group(1))
+    file_input.write(input3.group(1))
+
+
+file_input.close()
