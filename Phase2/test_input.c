6c5bb5916e9e58591c7ded0bf798414e6b22b2d4
Darshan D <darshand2000@gmail.com>
2021-10-04 10:15:20 +0530

    Test input and output program

---
+++ Phase2/test_input.c
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+int test(int a)
+{
+    return a + 2;
+}
+
+int abc = 2;
+
+int add(int a, int b)
+{
+    // test(1);
+    return a + b;
+    
+}
+
+
+
+int main(int argc, const char** argv)
+{
+    int res1;
+    int a1 = 3;
+    int b1 = 4;
+    res1 = add(a1,b1);
+    
+    
+    
+    // bind
+    // send it to thread
+    
+    
+    // x - > thread to add fn
+    // x.wait();
+    
+    int res2 = res1 + 5;
+    
+    res1 = res2 + 2;
+    
+    // for(x : void )
+    // {
+    //     x.wait();
+    // }
+    
+    
+}
+
+		
+		
+	
+
+
