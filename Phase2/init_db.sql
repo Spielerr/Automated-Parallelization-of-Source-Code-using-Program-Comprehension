517f929464fdc74873bebdcd2b4d50541a9f6fa1
Spielerr <kkgstud007@gmail.com>
2021-10-04 13:11:25 +0530

    Add initial sql script file to run to create db and insert some dummy values

---
+++ Phase2/init_db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE parallel_code;
+USE parallel_code;
+CREATE TABLE mapping;
+INSERT INTO mapping VALUES ('parallel_sort', 'code for parallel sort\n');
+INSERT INTO mapping VALUES ('parallel_max', 'code for parallel max\n');
